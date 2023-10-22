ee089d47724430675e1ad9896112fcce6ae8bbd9
Upsidedly <matthewlloydw@gmail.com>
2023-10-22 11:02:19 -0500

    feat: add interesting servers page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,43 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: '/upsided/',
-			social: {
-				discord: 'https://discord.gg/PKCTcBPB8r',
+  integrations: [
+    starlight({
+      title: "/upsided/",
+	//   logo: {
+	// 	src: './src/assets/3du.png',
+	// 	replacesTitle: false
+	//   },
+		favicon: {
+			
+		},
+      social: {
+        discord: "https://discord.gg/PKCTcBPB8r",
+      },
+	  editLink: {
+		baseUrl: 'https://github.com/Upsidedly/directory/edit/master/'
+	  },
+      sidebar: [
+        {
+          label: "Information",
+          items: [
+            {
+              label: "Rules",
+              link: "/rules/",
+            },
+			{
+				label: "Interesting Servers",
+				link: "/interesting-servers/"
 			},
-			sidebar: [
-				{
-					label: 'Rules',
-					link: '/rules/'
-				},
-				{
-					label: 'FAQ',
-					link: '/faq/'
-				}
-			],
-		}),
-	],
+            {
+              label: "FAQ",
+              link: "/faq/",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
